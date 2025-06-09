8f7a382c8422585497623f32b2c062ab07a2b398
Walter905-creator <pro4u.improvements@gmail.com>
2025-06-09 13:53:20 -0400

    Add basic Subscribe component

---
+++ client/src/client/src/components/Subscribe.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Subscribe() {
+  return (
+    <div>
+      <h2>Subscribe</h2>
+      <p>This is the Subscribe component.</p>
+    </div>
+  );
+}
+
+export default Subscribe;
