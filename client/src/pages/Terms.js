207852d1b2e163ea7ecb3304df13579f8a499de1
Walter905-creator <pro4u.improvements@gmail.com>
2025-06-09 15:28:31 -0400

    Create Terms.js
    Recreated pages folder and Terms.js

---
+++ client/src/pages/Terms.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Terms() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif", lineHeight: "1.6" }}>
+      <h1>Terms and Conditions</h1>
+      {/* Add your terms here */}
+      <p>By using Handyman Connect, you agree to these terms.</p>
+    </div>
+  );
+}
+
+export default Terms;
