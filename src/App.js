7bdb8faab1f5fb561dd307d5238ec8f03b5764ef
Walter905-creator <pro4u.improvements@gmail.com>
2025-07-23 06:55:57 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Fixlo</h1>
+      <p>This is the base React App structure ready to deploy on Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
